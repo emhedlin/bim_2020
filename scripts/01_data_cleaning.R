bd2f5be62f5dd7cd3f2429376217e8eefa5bb00e
emhedlin <hedlin@ualberta.ca>
2020-10-03 17:49:03 -0600

    added territory script, notice major discrepancies between 2019 report and data. Will develop 01 script to investigate

--- scripts/01_data_cleaning.R
+++ scripts/01_data_cleaning.R
@@ -3,15 +3,11 @@ library(tidyverse)
 
 # join 2020 survey data and historical ------------------------------------
 
-hist <- read_csv("data/2012-2020_occ.csv")
+hist <- read_csv("data/2012-2019_report.csv")
 current <- read_csv("data/2020_survey.csv") 
 db <- read_csv("data/2019_BIM_db.csv")
 current %>% filter(SiteID == "52")
-db_occ <- db %>% select(1, 66, 74, 82, 90, 98, 106, 114)
 
-
-occ %>% filter(SiteID == "52")
-unique(occ$SiteID)
 # clean 2020 survey data --------------------------------------------------
 names(current)
 current <- current %>% select("SiteID" = "Site",
@@ -20,16 +16,19 @@ current <- current %>% select("SiteID" = "Site",
                               "y20v3" = "20_Sp_3")
 
 full <- current %>% left_join(hist, by= "SiteID")
-full %>% filter(SiteID == "5")
+
 # filter out sites not included in 2019 analysis because they haven't been occupied since 2011
-full <- full %>% filter(!SiteID %in% c(5, 23, 52, 73, 104, 117)) 
+# full <- full %>% filter(!SiteID %in% c(5, 23, 52, 73, 104, 117)) 
 
 
 
 # tables -----------------------------
 
-occ <- full %>% select(1:4, 6:7, 20:42, -X35) 
-write.csv(occ, "data/2012-2020.csv")
+occ <- full %>% select(1:4, 6:7, 20:44, -X35) 
+
+write.csv(occ, "data/2012-2020_report.csv")
+
+
 occ <- occ %>% pivot_longer(cols = 2:ncol(occ)) %>% mutate(year = str_sub(name, 2,3), survey = str_sub(name, 5,5)) %>% 
   mutate(value = ifelse(value == "not detected" | is.na(value), 0, value)) %>%
   mutate(pefa = ifelse(value == "PEFA", 1, 0), rlha = ifelse(value == "RLHA", 1, 0))
