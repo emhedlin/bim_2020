38ff2b307b000f2aaedb1acd3f1714ce52bebf27
emhedlin <hedlin@ualberta.ca>
2020-11-12 10:09:42 -0500

    frequentist models for Al's comments

---
+++ 07_report_comments.R
@@ -0,0 +1,18 @@
+
+# Script addresses comments from Alastair about additional models he'd like to see:
+
+# the following in any framework
+# intercept
+# Intercept + dnon
+# Intercept + d2d
+# Intercept + NDVI
+# Intercept + dnon + d2d
+# Intercept + dnon + NDVI
+# Intercept + NDVI + d2d
+# Intercept + dnon + d2d + NDVI
+
+# the following in INLA
+# Top model + space
+# Top model + space + iid
+# Top model + space + AR1
+
